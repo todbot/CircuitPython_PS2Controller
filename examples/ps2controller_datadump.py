786563cbb46e9e3f679ead34dd956b70c07c15e3
Tod Kurt <todbot@users.noreply.github.com>
2023-06-09 14:21:45 -0700

    update examples with datadump

--- examples/ps2controller_datadump.py
+++ examples/ps2controller_datadump.py
@@ -7,7 +7,6 @@ import board
 from ps2controller import PS2Controller
 
 # one way to wire this up, for example on a Pico
-#ps2 = PS2Controller(dat=board.GP2, cmd=board.GP3, att=board.GP4, clk=board.GP5)
 ps2 = PS2Controller(dat=board.GP2, cmd=board.GP3, att=board.GP4, clk=board.GP5,
                     enable_pressure=True)
 
@@ -16,4 +15,4 @@ while True:
     events = ps2.update()
     print("dt:%2d" % int(ps2.last_dt*1000),
           "data:",' '.join("%2x" % v for v in ps2.data),
-          "events:",len(events))
+          "events:",len(events) if events is not None else 'None')
